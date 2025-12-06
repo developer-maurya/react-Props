01e0afc190c8a4229d4ee414c41b9946e9e25c53
Chitranjan kumar <chitranjan75075@gmail.com>
2025-12-06 23:22:19 +0530

    Add BoxMo component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,17 @@
+import BoxMo from "./component/boxMo"
+
+ 
+
+function App() {
+
+  return (
+    <>
+    <div> <p>hello l am developer</p></div>
+     <BoxMo/>
+    </>
+  )
+}
+
+export default App
+
 
